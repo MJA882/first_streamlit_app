0fc4905901aed24be2ca60ba61812902138b5db0
MJA882 <112200292+MJA882@users.noreply.github.com>
2022-08-31 08:09:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner Menu')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Idli Sambhar')
+streamlit.text('Medu Wada')
+streamlit.text('Tamarind Rice')
